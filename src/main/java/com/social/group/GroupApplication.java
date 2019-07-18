b3e16c5cfe461dd40f03edf028eb6fda53ad5b94
lailanelkoussy <lailanelkoussy@aucegypt.edu>
2019-07-18 14:30:00 +0200

    added support for eureka server

--- src/main/java/com/social/group/GroupApplication.java
+++ src/main/java/com/social/group/GroupApplication.java
@@ -2,12 +2,14 @@ package com.social.group;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableSwagger2
+@EnableDiscoveryClient
 public class GroupApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/social/group/proxies/UserServiceProxy.java
+++ src/main/java/com/social/group/proxies/UserServiceProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "UserService", url = "http://localhost:8081")
+@FeignClient(name = "UserService")
 @RequestMapping(value = "/users")
 public interface UserServiceProxy {
 
